837a969ef0f15859dbd931967de246d64b889afd
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-03-25 15:08:56 +0200

    ft: add tabs on the explore screen

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/colors.dart';
-
-import '../components/tabs_feature.dart';
+import 'package:movie_app/components/tabs/movies_tab.dart';
+import 'package:movie_app/components/tabs/tv_series_tab.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: SingleChildScrollView(
+    return SingleChildScrollView(
+        child: SafeArea(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
                 child: Column(
@@ -41,7 +41,64 @@ class ExploreScreen extends StatelessWidget {
                       shadowColor: WidgetStatePropertyAll(Colors.transparent)
                     ),
                     const SizedBox(height: 30),
-                    TabsFeature()
+                    DefaultTabController(
+                        length: 4,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              indicatorWeight: 1,
+                              indicatorSize: TabBarIndicatorSize.label,
+                              dividerColor: MColors.darkGrey,
+                              dividerHeight: 0.6,
+                              isScrollable: true,
+                              labelPadding: EdgeInsets.only(right: 40),
+                              tabAlignment: TabAlignment.start,
+                              labelStyle: TextStyle(
+                                color: MColors.primary,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w600
+                              ),
+                              indicatorColor: Colors.transparent,
+                              overlayColor: WidgetStatePropertyAll(Colors.transparent),
+                              tabs: [
+                                Tab(
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text("Movies"),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text("Tv Series"),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text("Documentary"),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text("Sports"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 900,
+                              child: TabBarView(children: [
+                                MoviesTab(),
+                                TvSeriesTab(),
+                                Text("Documentary"),
+                                Text("Sports"),
+                              ]),
+                            )
+                          ],
+                        )
+                    )
                   ],
                 )
             ),
