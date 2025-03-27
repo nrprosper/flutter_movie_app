88e8792ec3b47731096a442201427abc652ebaa9
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-03-27 11:09:59 +0200

    refactor: update the home carousel to navigate to series details when clicked

--- lib/components/home_carousel.dart
+++ lib/components/home_carousel.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app/data/tv_series.dart';
+import 'package:movie_app/screens/tv_details_screen.dart';
 import 'package:shimmer/shimmer.dart';
 import '../common/image_cache_widget.dart';
 import '../services/tmdb_service.dart';
@@ -78,37 +79,45 @@ class _HomeCarouselState extends State<HomeCarousel> {
         items: movies.map((movie) {
           return ClipRRect(
             borderRadius: BorderRadius.circular(10.0),
-            child: Stack(
-              children: [
-                Hero(
-                    tag: movie.id,
-                    child: TmdbImage(
-                      imagePath: movie.posterPath,
-                      width: double.infinity,
-                      height: double.infinity,
-                    )
-                ),
-                Positioned(
-                  bottom: 20,
-                  left: 20,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text(
-                        movie.originalName,
-                        style: GoogleFonts.lato(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600
+            child: InkWell(
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TvDetailsScreen(tv: movie))
+                )
+              },
+              child: Stack(
+                children: [
+                  Hero(
+                      tag: movie.id,
+                      child: TmdbImage(
+                        imagePath: movie.posterPath,
+                        width: double.infinity,
+                        height: double.infinity,
+                      )
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    left: 20,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          movie.originalName,
+                          style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           );
 
